64d885e1ec86bbf2d33c3c1eaed3b3e470150431
Rohit Chavan <53335793+rohitchavan-git@users.noreply.github.com>
2019-07-31 17:40:02 +0530

    init back end files

---
+++ springboot2-jpa-crud-example/src/main/java/com/rohit/chavan/spring/springboot2jpacrudexample/Springboot2JpaCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.chavan.spring.springboot2jpacrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2JpaCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2JpaCrudExampleApplication.class, args);
+	}
+
+}
---
+++ springboot2-jpa-crud-example/src/test/java/com/rohit/chavan/spring/springboot2jpacrudexample/Springboot2JpaCrudExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rohit.chavan.spring.springboot2jpacrudexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2JpaCrudExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
